8e7d757b86fb070289cbee701a3d2932957631ce
rbwrightjr91 <rbwrightjr91@gmail.com>
2018-04-03 19:53:17 -0700

    cleaning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logo from '../assets/images/logo.svg';
 
 const Footer = (props) => (
     <footer id="footer">
-        <section className="copyright">
+        <section className="main special">
             <h2>Find me</h2>
             <ul className="icons">
                 <li><a href="https://twitter.com/rbwrightjr91" target="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
