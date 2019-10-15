9f00d2b8b07b78e18a7ff708d88408c1b54b760b
Richard Wright <richard.wright@powerschool.com>
2019-10-14 19:45:06 -0700

    add last updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,6 +16,7 @@ const Footer = (props) => (
                 <li><a href="mailto:rbwrightjr91@gmail.com" target="#" className="icon fa-google alt"></a></li>
             </ul>
         
+        <p>Last updated: October 14th, 2019</p>
         <p className="copyright">Header icon made by <a href="https://www.flaticon.com/authors/becris" title="Becris">Becris</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a><br/>Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
         </section>
     </footer>
