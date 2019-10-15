49888a6772a3d01f8f2fd3686fd92f97843710c7
Richard Wright <richard.wright@powerschool.com>
2019-10-14 19:39:08 -0700

    add title

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={computer} alt="" /></span>
         <h1>Richard Wright</h1>
+        <p>Associate DevOps Engineer @ PowerSchool</p>
         <p>Computer Science @ CSU Sacramento</p>
     </header>
 )
