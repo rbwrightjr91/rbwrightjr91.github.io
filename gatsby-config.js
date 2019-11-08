ddc7af131c036ff240e815f9c5ecb9cc32d51a4e
Richard Wright <richard.wright@powerschool.com>
2019-11-08 15:44:44 -0800

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/posts`,
-        name: "posts",
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
       },
