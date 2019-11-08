6176cea4e5859856d8a4844a1062888b3508e22b
Richard Wright <richard.wright@powerschool.com>
2019-11-08 15:18:29 -0800

    Updates

---
+++ path---index-b4fd5a1e9bb18590462c.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{343:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Richard Wright - GitHub Pages"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-b4fd5a1e9bb18590462c.js.map
